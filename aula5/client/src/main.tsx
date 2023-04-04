22d2e515b8db6737dd3cc83f38548e70b6c9bb43
Vinicius Pegorini <viniciuspegorini@gmail.com>
2023-04-04 18:29:53 -0300

    Adicionadas as aulas 5 e 6.

--- aula5/client/src/main.tsx
+++ aula5/client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { App } from "./App";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "./context/AuthContext";
 
